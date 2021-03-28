7269629b7568b7f8bbb6636a8f47789044eea26e
Pedro Santos <psantos@zendesk.com>
2021-03-29 00:18:27 +0100

    Adding initial React components and dependencies

---
+++ app/javascript/components/const/index.jsx
@@ -0,0 +1,9 @@
+const SMALL_BIKE_LIST = 'smallBikes';
+const MEDIUM_BIKE_LIST = 'mediumBikes';
+const LARGE_BIKE_LIST = 'largeBikes';
+
+export {
+  SMALL_BIKE_LIST,
+  MEDIUM_BIKE_LIST,
+  LARGE_BIKE_LIST,
+};
